e9b63b721bf9c455fa573ecb51efb6dd5cb2c478
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-10-13 13:08:27 +0300

    add locale list and dictionary

---
+++ src/text-configs/hero.js
@@ -0,0 +1,38 @@
+export default {
+  "hero-carousel-header": {
+    "ru": "Fruit Service",
+    "en": "Fruit Service Group"
+  },
+  "hero-carousel-subheader": {
+    "ru": "Оптовые поставки и газация фруктов и овощей",
+    "en": ""
+  },
+  "connect": {
+    "ru": "Связаться",
+    "en": "Connect"
+  },
+  "hero-features-1": {
+    "ru": "Выгодные цены",
+    "en": "Connect"
+  },
+  "hero-features-2": {
+    "ru": "Широкий ассортимент",
+    "en": ""
+  },
+  "hero-features-3": {
+    "ru": "Оперативная обработка и исполнение заказа",
+    "en": ""
+  },
+  "hero-features-4": {
+    "ru": "Своевременная доставка",
+    "en": ""
+  },
+  "hero-features-5": {
+    "ru": "Индивидуальный подход к каждому клиенту",
+    "en": ""
+  },
+  "hero-features-6": {
+    "ru": "Высочайшее качество",
+    "en": ""
+  }
+}
