3756ae094ed6f3ac87cf0ddfff25145d9ebe7bff
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-10-13 13:10:04 +0300

    add Root component

---
+++ src/components/Root.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {Provider} from 'react-redux';
+import Header from './Header';
+import Hero from './Hero';
+import LanguageProvider from './LanguageProvider';
+
+class Root extends Component {
+	static propTypes = {};
+
+	static childContextTypes = {
+		lang: PropTypes.string,
+	};
+
+	state = {
+		lang: 'ru'
+	};
+
+	getChildContext() {
+		return {
+			lang: this.state.lang
+		};
+	}
+
+	render() {
+		console.log(this.state);
+		return (
+			<LanguageProvider lang={this.state.lang}>
+				<div className="mainContainer">
+					<Header handleLangChange = {this.handleLangChange}/>
+					<Hero/>
+				</div>
+			</LanguageProvider>
+		);
+	}
+
+	handleLangChange = (lang) => {
+		this.setState({lang: lang});
+	}
+}
+
+export default Root;
