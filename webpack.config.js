e46b808d47ee24c3e2196d30c012e358cfc549ec
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-10-20 13:55:43 +0300

    add modules for assets linking add plugin for copying images

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
-const pluginsArray = [new webpack.HotModuleReplacementPlugin()];
+const pluginsArray = [
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyWebpackPlugin([
+        { from: 'src/images', to:'images' }
+    ])
+];
 
 if (isProduction) pluginsArray.push(new MiniCssExtractPlugin());
 
@@ -10,25 +16,31 @@ if (isProduction) pluginsArray.push(new MiniCssExtractPlugin());
 module.exports = {
     entry: './src/index.js',
     module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: ['babel-loader']
-        },
-        {
-            test: /\.less$/,
-            use: [
-                {
-                    loader: isProduction ? MiniCssExtractPlugin.loader : 'style-loader'
-                },
-                {
-                    loader: 'css-loader'
-                },
-                {
-                    loader: 'less-loader'
-                }
-            ]
-        }]
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(less|css)$/,
+                use: [
+                    {
+                        loader: isProduction ? MiniCssExtractPlugin.loader : 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'less-loader'
+                    }
+                ]
+            },
+            { 
+                test: /\.(gif|woff|woff2|eot|ttf|svg)$/, 
+                loader: 'url-loader?limit=100000'
+            }
+        ]
     },
     resolve: {
         extensions: ['*', '.js', '.jsx']
