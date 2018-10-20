79102824fa3d5fd0bddeab61464a6b1cc91e0ea5
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-10-20 13:59:50 +0300

    edit header - improve navigation, language select

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 import localeList from '../text-configs/lang-list';
 import Select from 'react-select';
 import PropTypes from 'prop-types';
+import Translator from './Translator';
 
 class Header extends Component {
 	static propTypes = {
 		handleLangChange: PropTypes.func
 	};
 
+	handleChange = (selected) => {
+		this.props.handleLangChange(selected.value);
+	};
+
 	render() {
-		console.log(localeList);
-		const langList = localeList.map(value => (
+		const langList = Object.keys(localeList).map(value => (
 			{
 				value,
-				label: value
+				label: localeList[value]
 			}
 		));
 
@@ -21,19 +25,31 @@ class Header extends Component {
 
 		return(
 			<header>
-				<a href="/">Logo</a>
-				<nav><li></li></nav>
-				<Select 
-					options = {langList}
-					onChange = {this.handleChange}
-					value = {selected}
-				/>
+				<div className="logoBlock">
+					<a href="/" className="logo">
+						<img src="images/fruit-service-group-logo-min.png" alt="Fruit Service Group"/>
+					</a>
+				</div>
+				<nav>
+					<ul>
+						<li><a href="#about"><Translator>header-about</Translator></a></li>
+						<li><a href="#product"><Translator>header-product</Translator></a></li>
+						<li><a href="#services"><Translator>header-services</Translator></a></li>
+						<li><a href="#contacts"><Translator>header-contacts</Translator></a></li>
+						<li>
+							<Select 
+								options={langList}
+								onChange={this.handleChange}
+								value={selected}
+								className="langSelect"
+								defaultValue={langList[0]}
+								classNamePrefix="langSelect"
+							/>
+						</li>
+					</ul>
+				</nav>
 			</header>
 		);
-	}
-
-	handleChange = (selected) => {
-		this.props.handleLangChange(selected.value);
 	}
 
 }
