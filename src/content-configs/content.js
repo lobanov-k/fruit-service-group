3f8b2a2f67da9ce19f6952fd1daaac9e4bbd813f
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-10-22 00:10:01 +0300

    add content config

---
+++ src/content-configs/content.js
@@ -0,0 +1,7 @@
+export const svgSet1 = [
+    '../images/svg/027-avocado.svg',
+    '../images/svg/001-tomato.svg',
+    '../images/svg/023-pepper.svg',
+    '../images/svg/015-pomegranate.svg',
+    '../images/svg/019-kiwi.svg'
+];
