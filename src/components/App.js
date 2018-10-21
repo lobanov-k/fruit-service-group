9196939fa0b32477ed138db73bfde87ec022a645
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-10-22 00:10:34 +0300

    add info block

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import Header from './Header';
 import Hero from './Hero';
 import LanguageProvider from './LanguageProvider';
+import Translator from './Translator';
 import CompanyBlock from './CompanyBlock';
+import InfoBlock from './InfoBlock';
+
+import {svgSet1} from '../content-configs/content.js';
 
 class App extends Component {
 	static childContextTypes = {
@@ -31,6 +35,10 @@ class App extends Component {
 					<Header handleLangChange = {this.handleLangChange}/>
 					<Hero/>
 					<CompanyBlock/>
+					<InfoBlock svgArray={svgSet1} bgColor="#EBC809" bgImage="/images/content/9.jpg">
+						<div className="textBlock"><Translator>info-block-text-1</Translator></div>
+						<div className="textBlock"><Translator>info-block-text-2</Translator></div>
+					</InfoBlock>
 				</div>
 			</LanguageProvider>
 		);
