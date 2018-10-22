e8d5b5e880d56a1054683feb166b13862b98bfc8
Konstantin Lobanov <konstantin.lobanov@ab-soft.net>
2018-10-22 17:34:43 +0300

    add new component

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import LanguageProvider from './LanguageProvider';
 import Translator from './Translator';
 import CompanyBlock from './CompanyBlock';
 import InfoBlock from './InfoBlock';
+import TextHeadingSection from './TextHeadingSection';
 
 import {svgSet1} from '../content-configs/content.js';
 
@@ -39,6 +40,15 @@ class App extends Component {
 						<div className="textBlock"><Translator>info-block-text-1</Translator></div>
 						<div className="textBlock"><Translator>info-block-text-2</Translator></div>
 					</InfoBlock>
+					<TextHeadingSection>
+						<h3 className="heading">
+							<Translator>product-head</Translator>
+						</h3>
+						<div className="text">
+							<p><Translator>product-text-1</Translator></p>
+							<p><Translator>product-text-2</Translator></p>
+						</div>
+					</TextHeadingSection>
 				</div>
 			</LanguageProvider>
 		);
