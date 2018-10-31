872547da63576db169cf02a2cef2c6215afaa5a0
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-11-01 01:00:32 +0200

    add component: Columns

---
+++ src/components/Columns.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    children: PropTypes.node.isRequired,
+    title: PropTypes.string
+};
+
+export default function Columns(props) {
+    return (
+        <React.Fragment>
+            <div className="columnsBlock">
+                <div className="titleBlock">
+                    <div className="container">
+                        <div className="columnTitle">
+                            {props.title}
+                        </div>
+                    </div>
+                </div>
+                <div className="mainColumns">
+                    <div className="container">
+                        {props.children}
+                    </div>
+                </div>
+                <div className="titleBlock">
+                    <div className="container">
+                        <div className="columnTitle"/>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+Columns.propTypes = propTypes;
