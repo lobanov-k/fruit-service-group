ca30d1d3db63e07b2da698f018152d4774974f29
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-11-02 23:54:52 +0200

    refactor CompanyBlock and add scrollable anchors

--- src/components/CompanyBlock.js
+++ src/components/CompanyBlock.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Translator from './Translator';
+import ScrollableAnchor from 'react-scrollable-anchor';
 
-const propTypes = {};
-
-const defaultProps = {};
-
-export default function CompanyBlock(props) {
+export default function CompanyBlock() {
 	return (
 		<React.Fragment>
-			<div className="companyBlock">
-				<div className="container">
-					<div className="titleBlock">
-						<h2>
-							<Translator>company-header</Translator>
-						</h2>
-					</div>
-					<div className="textBlock">
-						<div className="main"><Translator>company-text-main</Translator></div>
-						<a href="#more" className="moreLink"><Translator>company-more</Translator></a>
-						<div className="companyInfo"><Translator>company-info</Translator></div>
+			<ScrollableAnchor id={"about"}>
+				<div className="companyBlock">
+					<div className="container">
+						<div className="titleBlock">
+							<h2>
+								<Translator>company-header</Translator>
+							</h2>
+						</div>
+						<div className="textBlock">
+							<div className="main"><Translator>company-text-main</Translator></div>
+							<a href="#more" className="moreLink"><Translator>company-more</Translator></a>
+							<div className="companyInfo"><Translator>company-info</Translator></div>
+						</div>
 					</div>
 				</div>
-			</div>
+			</ScrollableAnchor>
 		</React.Fragment>
 	);
 }
-
-CompanyBlock.propTypes = propTypes;
-CompanyBlock.defaultProps = defaultProps;
