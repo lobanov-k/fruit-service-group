916af7798093b351eb144019c597bf287f38653a
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-11-06 23:11:52 +0200

    Translator can return text without tags and tags with classes

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -4,29 +4,36 @@ import dictionary from '../text-configs/dictionary';
 
 class Translator extends Component {
     static propTypes = {
-        children: PropTypes.string
+        children: PropTypes.string,
+        className: PropTypes.string,
+        isTag: PropTypes.bool
     };
 
     static contextTypes = {
         lang: PropTypes.string.isRequired
     };
-    
+
     render() {
         if (typeof this.props.children != 'string') {
             console.error('Translator needs string children', 'Property passed:', this.props.children);
             return ( this.props.children );
         }
-        
+
         if (!dictionary[this.props.children]) {
             console.error('No entry in dictionary', 'Property passed:', this.props.children);
             return ( this.props.children );
         }
 
-        return (
-            <span>
-                { dictionary[this.props.children][this.context.lang] }
-            </span>
-        );
+        if ( typeof this.props.isTag == 'undefined' || this.props.isTag === true) {
+            return (
+                <span className={this.props.className || ''}>
+                    { dictionary[this.props.children][this.context.lang] }
+                </span>
+            );
+        } else {
+            return (<React.Fragment>{dictionary[this.props.children][this.context.lang]}</React.Fragment>);
+        }
+
     };
 }
 
