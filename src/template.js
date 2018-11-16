312a88f465ddb720d6ebfe5657dcb2819cd0bbb2
Konstantin Lobanov <konstantin.lobanov@ab-soft.net>
2018-11-16 18:52:02 +0200

    Template for serverside rendering

--- src/template.js
+++ src/template.js
@@ -1,34 +1,36 @@
-function template(seo, content = ""){
-const {title, description, keyWords} = seo;
-let scripts = '';
-if(content){
-    scripts = ` <script>
-        window.__STATE__ = ${JSON.stringify(initialState)}
-    </script>
-    <script src="assets/client.js"></script>
-    `;
-} else {
-    scripts = ` <script src="assets/bundle.js"> </script> `
-}
-let page = `<!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="utf-8">
-            <title> ${title} </title>
-            <meta description="${description}" />
-            <meta keywords="${keyWords}"/>
-            <link href="assets/style.css" rel="stylesheet">
-        </head>
-        <body>
-            <div class="content">
-                <div id="app" class="wrap-inner">
-                    <!--- magic happens here -->  ${content}
+function template(seo, initialState = {}, content = ""){
+    const {title, description, keyWords} = seo;
+    let scripts = '';
+    if(content){
+        scripts = ` <script>
+            window.__STATE__ = ${JSON.stringify(initialState)}
+        </script>
+        <script src="assets/client.js"></script>
+        `;
+    } else {
+        scripts = ` <script src="assets/bundle.js"> </script> `
+    }
+    let page = `<!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <title> ${title} </title>
+                <meta description="${description}" />
+                <meta keywords="${keyWords}"/>
+                <link href="assets/style.css" rel="stylesheet">
+            </head>
+            <body>
+                <div class="content">
+                    <div id="app" class="wrap-inner">
+                        <!--- magic happens here -->  ${content}
+                    </div>
                 </div>
-            </div>
-            ${scripts}
-        </body>
-    </html>
-`;
+                ${scripts}
+            </body>
+        </html>
+    `;
+
+    return page;
 }
 
 module.exports = template;
