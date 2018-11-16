8142949e1f61a8b57cd2ffa4ab8f338c63982594
Konstantin Lobanov <konstantin.lobanov@ab-soft.net>
2018-11-16 18:52:21 +0200

    Configs for SEO tags

---
+++ src/content-configs/seo.js
@@ -0,0 +1,5 @@
+module.exports = {
+	title: "Fruit Service Group",
+	description: "Fruit Service Group - Freight forwarding and customs clearance services.",
+	keyWords: "Fruits, vegetables, forwarding, shipping"
+};
