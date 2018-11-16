98c8b3c2a42df676aa2a47148b8592fd5c49ce1b
Konstantin Lobanov <konstantin.lobanov@ab-soft.net>
2018-11-16 18:50:22 +0200

    Add serverside and clientside react builds.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,10 @@
-const express = require("express");
-const app = express();
+import React from 'react';
+import { renderToString } from 'react-dom/server'
+import App from './components/App';
 
-app.get("/", (request, response) => {
-    response.sendFile(__dirname + "/dist/index.html");
-});
-
-app.get("/en/", (request, response) => {
-    response.sendFile(__dirname + "/dist/index.html");
-});
-
-app.listen(8888, () => {
-    console.log("Server running at port 8888: http://127.0.0.1:8888");
-});
+module.exports = function render(initialState) {
+    let content = renderToString(
+        <App {...initialState}/>
+    );
+    return content;
+}
