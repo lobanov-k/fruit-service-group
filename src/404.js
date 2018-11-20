e8bf0cd88caf086c73268affab906e73f3002028
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-11-21 00:44:39 +0200

    Customize 404

--- src/404.js
+++ src/404.js
@@ -13,15 +13,46 @@ function notFound(lang = 'ru') {
         
     };
 
-    const { title, header, linkText} = (lang == 'ru') ? contentRu : contentEn;
+    const { title, header, linkText, homepage} = (lang == 'ru') ? contentRu : contentEn;
     const page = `<!DOCTYPE html>
         <html lang="${lang}">
             <head>
                 <meta charset="utf-8">
                 <title> ${title} </title>
+                <style>
+                    body {
+                        background-color: #fff;
+                        text-align: center;
+                        font-family: "Roboto", "Lucida Grande", Arial, Helvetica, sans-serif;
+                    }
+                    .logo {
+                        display: block;
+                        margin: 30px auto;
+                    }
+                    .logo img {
+                        height: 50px;
+                        width: auto;
+                    }
+                    h1 {
+                        font-size: 36px;
+                        line-height: 1;
+                        font-weight: bold;
+                        color: #000;
+                        margin-bottom: 20px;
+                    }
+                    .backLink {
+                        color: #467035;
+                    }
+                    .content {
+                        padding-top: 50px;
+                    }
+                </style>
             </head>
             <body>
                 <div class="content">
+                    <a href="${homepage}" class="logo">
+                        <img src="images/fruit-service-group-logo-min.png" alt="Fruit Service Group"/>
+                    </a>
                     <h1>${header}</h1>
                     <a href="${homepage}" class="backLink">${linkText}</a>
                 </div>
