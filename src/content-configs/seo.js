59da1b8ff6b122cd003a00cb581e7b62891c2d33
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-11-21 00:22:36 +0200

    Add multilng SEO configurations

--- src/content-configs/seo.js
+++ src/content-configs/seo.js
@@ -1,5 +1,12 @@
 module.exports = {
-	title: "Fruit Service Group",
-	description: "Fruit Service Group - Freight forwarding and customs clearance services.",
-	keyWords: "Fruits, vegetables, forwarding, shipping"
+	'en': {
+		title: "Fruit Service Group",
+		description: "Fruit Service Group - Freight forwarding and customs clearance services.",
+		keyWords: "Fruits, vegetables, forwarding, shipping"
+	},
+	'ru': {
+		title: "Fruit Service Group",
+		description: "Fruit Service Group - Оптовые поставки и газация фруктов и овощей.",
+		keyWords: "Фрукты, овощи, форвардинг, шипинг"
+	}
 };
