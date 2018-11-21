abdcf5a0f05e04a45a7de3880eddbebb0b985d37
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-11-21 23:14:11 +0200

    Use helmet package for security.

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const express = require('express'),
 	app = express(),
 	template = require('./views/template')
 	path = require('path'),
-	seo = require('./views/content-configs/seo');
+	seo = require('./views/content-configs/seo'),
+	var helmet = require('helmet');
 
 app.use(express.static(path.resolve(__dirname, '/www')));
 // Serving static files
+app.use(helmet());
 app.use('/fonts', express.static(path.resolve(__dirname, '/fonts')));
 app.use('/images', express.static(path.resolve(__dirname, '/images')));
 app.use('/js', express.static(path.resolve(__dirname, '/js')));
 app.use('/css', express.static(path.resolve(__dirname, '/css')));
 
-// hide powered by express
-app.disable('x-powered-by');
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
 // start the server
 app.listen(6666);
 
@@ -30,6 +33,11 @@ app.get('/en', (req, res) => {
 	const response = template(seo, {initialLang: 'en'}, content);
 	res.setHeader('Cache-Control', 'assets, max-age=604800');
 	res.send(response);
+});
+app.post('/request', (req, res) => {
+	const {name, phone, message} = req.body;
+	console.log(name, phone, message);
+	res.send('success');
 });
 
 app.use(function(req, res){
