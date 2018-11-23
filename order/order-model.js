dca0b4f4a7996293c0ca065c104f11485c11a25e
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-11-23 22:08:57 +0200

    Order model connected to database.

--- order/order-model.js
+++ order/order-model.js
@@ -1,6 +1,16 @@
+const db = require('../db/db-connection');
+
 class Order {
     static create(name, phone, message) {
-        console.log(...arguments);
+        const sql = 'INSERT INTO `order` SET ?';
+        db.query(sql, {name, phone, message, date: new Date}, function (error, results, fields) {
+            if (error) {
+                throw error;
+            } else {
+                console.log(results.insertId);
+            }
+        });
+        db.end();
     }
 }
 
