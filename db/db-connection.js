8c1adbd263e2ecac3ac040c9b68727d2fc1dcfac
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-11-23 21:48:41 +0200

    Database connection function.

---
+++ db/db-connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+let dbSettings;
+
+try {
+    dbSettings = require('database');
+} catch (e) {
+    console.error(e);
+    console.log('Used database-sample.js');
+    dbSettings = require('database-sample');
+}
+
+const db = mysql.createConnection(dbSettings);
+
+module.exports = db;
