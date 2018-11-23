1a3500c2322db6788223b294de63fbfc05e66796
Konstantin Lobanov <lobanov.k.k@gmail.com>
2018-11-23 15:41:35 +0200

    Add backend sanitazation of request.

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const helmet = require('helmet');
-const expressValidator = require('express-validator');
+const { sanitizeBody } = require('express-validator/filter');
 const { check, oneOf, validationResult } = require('express-validator/check');
 
 const template = require('./views/template');
 const seo = require('./views/content-configs/seo');
 const { createOrder } = require('./order/order-controller');
-
-console.log(createOrder);
 
 app.use(express.static(path.resolve(__dirname, '/www')));
 // Serving static files
@@ -21,7 +19,6 @@ app.use('/css', express.static(path.resolve(__dirname, '/css')));
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(expressValidator()); 
 
 // start the server
 app.listen(6666);
@@ -42,12 +39,14 @@ app.get('/en', (req, res) => {
 	res.send(response);
 });
 app.post('/order',
-	// validation
-	oneOf([
-		check('name', 'Name must be a string').isString(),
-		check('phone', 'Phone must be a string').isString(),
-		check('message', 'Message must be a string').isString()
-	]),
+	[
+		sanitizeBody(['name', 'phone', 'message']).escape().trim(),
+		oneOf([
+			check('name', 'Name must be a string').exists({checkFalsy: true}).isString(),
+			check('phone', 'Phone must be a string').exists({checkFalsy: true}).isString(),
+			check('message', 'Message must be a string').exists({checkFalsy: true}).isString()
+		])
+	],
 	(req, res, next) => {
 		try {
 			validationResult(req).throw();
